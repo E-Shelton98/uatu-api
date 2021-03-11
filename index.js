757c7a9ca1f026d0af3cf8984560f05185315fd3
E-Shelton98 <erik.shelton92@gmail.com>
2021-03-11 00:53:31 -0500

    moved dotenv to dev dependency and configured

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //require express
 const express = require('express')
+//require dotenv if environment = development
+if (NODE_ENV='development') {
+    const dotenv = require('dotenv')
+    dotenv.config()
+}
 //set app to express()
 const APP = express()
 //set port to 3000 || env
@@ -9,4 +14,4 @@ APP.use(express.json())
 //set get route to 'Hello World'
 APP.get('/', (req, res) => res.json({ message: 'Hello World'}))
 //tell the app to listen on our port variable
-APP.listen(port, () => console.log(`Listening on port ${PORT}`))
+APP.listen(PORT, () => console.log(`Listening on port ${PORT}`))
