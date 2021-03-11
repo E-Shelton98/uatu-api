f87031afa32cc066495137e89ed89292d754c91f
E-Shelton98 <erik.shelton92@gmail.com>
2021-03-11 00:50:13 -0500

    basic setup complete

---
+++ index.js
@@ -0,0 +1,12 @@
+//require express
+const express = require('express')
+//set app to express()
+const APP = express()
+//set port to 3000 || env
+const PORT = process.env.PORT || 3000
+//tell the app to accept JSON body in requests
+APP.use(express.json())
+//set get route to 'Hello World'
+APP.get('/', (req, res) => res.json({ message: 'Hello World'}))
+//tell the app to listen on our port variable
+APP.listen(port, () => console.log(`Listening on port ${PORT}`))
